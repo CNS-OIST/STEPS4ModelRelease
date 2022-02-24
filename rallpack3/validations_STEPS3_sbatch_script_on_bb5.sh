3d380e0e117272e7fe0b6887bfa3fbe99da70364
Alessandro Cattabiani <alessandro.cattabiani@epfl.ch>
2022-02-24 16:52:58 +0100

    fixup! scripts 2

--- rallpack3/validations_STEPS3_sbatch_script_on_bb5.sh
+++ rallpack3/validations_STEPS3_sbatch_script_on_bb5.sh
@@ -15,7 +15,8 @@ module load archive/2021-12 python-dev
 . ../../../oldspack/share/spack/setup-env.sh
 spack load /saf25ux
 
-ntasks=$((${SLURM_JOB_NUM_NODES} * 32))
+nodes=$SLURM_JOB_NUM_NODES
+ntasks=$(($nodes * 32))
 seed=$(($SLURM_ARRAY_TASK_ID * 1))
-time srun --nodes=$nodes --ntasks=${ntasks} dplace python rallpack3.py ${seed} mesh/axon_cube_L1000um_D866nm_1135tets.msh 0
+time srun --nodes=$nodes --ntasks=$ntasks dplace python rallpack3.py $seed mesh/axon_cube_L1000um_D866nm_1135tets.msh 0
 
