2eb1d65fe8a006930a36675afb38c7f80adaffda
Alessandro Cattabiani <alessandro.cattabiani@epfl.ch>
2022-02-24 16:45:34 +0100

    adapt rallpack3 scripts

--- rallpack3/rallpack3.py
+++ rallpack3/rallpack3.py
@@ -229,5 +229,5 @@ sim.run(SIM_END)
 
 if MPI.rank == 0:
     df = pd.DataFrame({"t":Vrs.time[0], "z_min":Vrs.data[0,:,0], "z_max":Vrs.data[0,:,1]})
-    df.to_csv(f'results/res{SEED}_STEPS{4 if USE_STEPS_4 else 3}.txt', sep=" ", index=False)
+    df.to_csv(f'results/STEPS{4 if USE_STEPS_4 else 3}/res{SEED}_STEPS{4 if USE_STEPS_4 else 3}.txt', sep=" ", index=False)
 
---
+++ rallpack3/validations_STEPS4_sbatch_script_on_bb5.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# The line above is a "she-bang" to tell that /bin/bash is the command interpreting this file
+
+#SBATCH --array=[1]
+#SBATCH --partition=prod
+#SBATCH --account=proj95
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=2:00:00
+#SBATCH --exclusive
+
+set -x
+
+module load archive/2021-12 python-dev
+. ../../../oldspack/share/spack/setup-env.sh
+spack load /saf25ux
+
+ntasks=$(($nodes * 32))
+seed=$(($SLURM_ARRAY_TASK_ID * 1))
+time srun --nodes=$nodes --ntasks=${ntasks} dplace python rallpack3.py ${seed} mesh/axon_cube_L1000um_D866nm_1135tets.msh 1
