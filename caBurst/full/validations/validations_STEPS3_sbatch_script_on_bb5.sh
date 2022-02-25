a78e1e0a57fe7b2d0b0351f04ca070f8203f53b9
Alessandro Cattabiani <alessandro.cattabiani@epfl.ch>
2022-02-25 14:02:27 +0100

    moving scripts and making caburst array 1-100

--- caBurst/full/validations/validations_STEPS3_sbatch_script_on_bb5.sh
+++ caBurst/full/validations/validations_STEPS3_sbatch_script_on_bb5.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-
-
 # You need to set the appropriate SBATCH_PARTITION, SBATCH_ACCOUNT
 
-#SBATCH --array=[1]
+#SBATCH --array=[1-100]
 #SBATCH --partition=$SBATCH_PARTITION
 #SBATCH --account=$SBATCH_ACCOUNT
 #SBATCH --nodes=32
@@ -20,4 +18,5 @@ nodes=$SLURM_JOB_NUM_NODES
 ntasks=$(($nodes * 32))
 seed=$(($SLURM_ARRAY_TASK_ID * 1))
 mesh_fls=../../mesh/split_1024/steps3/CNG_segmented_2_split_1024.msh
+
 time srun --nodes=$nodes --ntasks=$ntasks dplace python caBurstFullModel.py $seed $mesh_fls 0
