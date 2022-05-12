e311caac44f14079171e34e3520253b148ca5cca
Alessandro Cattabiani <alessandro.cattabiani@epfl.ch>
2022-05-12 16:51:58 +0200

    rallpack1 direct, rallpack3 GB, caburst direct

--- caBurst/full/validations/caBurstFullModel.py
+++ caBurst/full/validations/caBurstFullModel.py
@@ -203,7 +203,7 @@ def run(seed, mesh_path, steps_version):
     rng = RNG('mt19937', 512, seed)
 
     if steps_version == 4:
-        sim = Simulation('DistTetOpSplit', mdl, mesh, rng, searchMethod=NextEventSearchMethod.GIBSON_BRUCK)
+        sim = Simulation('DistTetOpSplit', mdl, mesh, rng, searchMethod=NextEventSearchMethod.DIRECT)
     else:
         part = GmshPartition(mesh)
         sim = Simulation('TetOpSplit', mdl, mesh, rng, MPI.EF_DV_PETSC, part)
