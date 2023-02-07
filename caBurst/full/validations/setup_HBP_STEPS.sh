6cc2c7346973b30e5c5e8db44c1f06305e3bff07
Katta <alessandro.cattabiani@epfl.ch>
2023-02-07 11:22:39 +0100

    WIP

--- caBurst/full/validations/setup_HBP_STEPS.sh
+++ caBurst/full/validations/setup_HBP_STEPS.sh
@@ -6,17 +6,12 @@ module purge
 
 rm -rf spackenv
 
-
-
-module load archive/2022-08
-
 git stash
 git checkout 0a7f75aa
 
 spack env create -d spackenv
 sed -i '6 i\  concretization: together' spackenv/spack.yaml
 spack env activate -d spackenv
-spack add petsc@3.16.1
 spack add steps@develop
-spack develop -p ${PWD} --no-clone steps@develop^petsc@3.16.1
+spack develop -p ${PWD} --no-clone steps@develop
 spack install
