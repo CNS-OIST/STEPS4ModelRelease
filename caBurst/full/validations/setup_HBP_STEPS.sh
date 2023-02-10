464b50da54f8c77bfc438c97f35b4b33f7ee706f
Katta <alessandro.cattabiani@epfl.ch>
2023-02-10 11:07:06 +0100

    setup for bisect

--- caBurst/full/validations/setup_HBP_STEPS.sh
+++ caBurst/full/validations/setup_HBP_STEPS.sh
@@ -6,11 +6,15 @@ module purge
 
 rm -rf spackenv
 
+
+
 module load archive/2022-08
+
 
 spack env create -d spackenv
 sed -i '6 i\  concretization: together' spackenv/spack.yaml
 spack env activate -d spackenv
+spack add petsc@3.16.1
 spack add steps@develop
-spack develop -p ${PWD} --no-clone steps@develop
+spack develop -p ${PWD} --no-clone steps@develop^petsc@3.16.1
 spack install
