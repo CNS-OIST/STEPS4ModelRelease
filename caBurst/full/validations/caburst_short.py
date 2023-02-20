ac1ba223435be08e8e4d2865e3846a464ef1dffc
Katta <alessandro.cattabiani@epfl.ch>
2023-02-20 10:48:40 +0100

    strict test STEPS3

--- caBurst/full/validations/caburst_short.py
+++ caBurst/full/validations/caburst_short.py
@@ -21,7 +21,7 @@ class SimulationError(Exception):
 
 def run(seed, mesh_path, steps_version):
 
-    par.NTIMEPOINTS = 30
+    par.NTIMEPOINTS = 50
 
     seed, steps_version = int(seed), int(steps_version)
 
